aa50a4d7dd5ae343b6b72b953e40d281c9349f11
Henry <henryjboles@gmail.com>
2018-02-13 21:41:37 -0800

    Update main.rb

--- main.rb
+++ main.rb
@@ -161,7 +161,7 @@ class Command
            s = "Paid"
         end
         mem = event.author
-        conc = "**" + mem.nick + "**'s Stats```Tax: $#{}\nBalence: $#{sprintf "%.2f", getVals(mem, :bal).to_f * 0.01}\nDaily Reward: $#{sprintf "%.2f", getVals(mem, :daily).to_f * 0.01}\nTax Rate: $#{sprintf "%.2f", getVals(mem, :taxamt).to_f * 0.01} per message\nInvestments: #{getVals(mem, :invest).to_s}```"
+        conc = "**" + mem.nick + "**'s Stats```Tax: #{"$"+s}\nBalence: $#{sprintf "%.2f", getVals(mem, :bal).to_f * 0.01}\nDaily Reward: $#{sprintf "%.2f", getVals(mem, :daily).to_f * 0.01}\nTax Rate: $#{sprintf "%.2f", getVals(mem, :taxamt).to_f * 0.01} per message\nInvestments: #{getVals(mem, :invest).to_s}```"
       end
       event.message.mentions.each do |mem|
         s = ""
@@ -171,7 +171,7 @@ class Command
            s = "Paid"
         end
         mem = mem.on(event.channel.server)
-        conc = "**" + mem.nick + "**'s Stats```Tax: $#{sprintf "%.2f", getVals(mem, :tax).to_f * 0.01}\nBalence: $#{sprintf "%.2f", getVals(mem, :bal).to_f * 0.01}\nDaily Reward: $#{sprintf "%.2f", getVals(mem, :daily).to_f * 0.01}\nTax Rate: $#{sprintf "%.2f", getVals(mem, :taxamt).to_f * 0.01} per message\nInvestments: #{getVals(mem, :invest).to_s}```"
+        conc = "**" + mem.nick + "**'s Stats```Tax: #{"$"+s}\nBalence: $#{sprintf "%.2f", getVals(mem, :bal).to_f * 0.01}\nDaily Reward: $#{sprintf "%.2f", getVals(mem, :daily).to_f * 0.01}\nTax Rate: $#{sprintf "%.2f", getVals(mem, :taxamt).to_f * 0.01} per message\nInvestments: #{getVals(mem, :invest).to_s}```"
       end
       event.respond conc
     end
