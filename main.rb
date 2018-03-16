bda511a94a4dfbd64d3240a5ec1ca086aae6475b
Henry <henryjboles@gmail.com>
2018-03-16 11:52:52 -0700

    Update main.rb

--- main.rb
+++ main.rb
@@ -352,6 +352,8 @@ class Command
       $conn.exec_params("select userid, #{type} from users where serverid=$1 and state!=1 and userid ~ '[0123456789]+' order by #{type} desc limit 10", [event.channel.server.id]) do |result|
         a = 1
         result.each do |row|
+          puts r["userid"]
+          puts event.channel.server.member(r["userid"].to_i)
           r = row
           out << "\n #{a.to_s}. #{event.channel.server.member(r["userid"].to_i).name} - #{ if ["invest","lbcount"].include? type then "#{r[type].to_s}" else "$#{r[type].mon.to_s}" end}"
           a = a + 1
